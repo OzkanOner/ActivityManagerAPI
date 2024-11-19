5a7369290c61cf18028a9a3562b79a9fc6a07b6a
Özkan Öner <ozkan@ozkanoner.com>
2024-11-19 20:30:27 +0300

    feat: added activity repository

---
+++ Repositories/Concrete/ActivityRepository.cs
@@ -0,0 +1,13 @@
+﻿using ActivityManagerAPI.Models;
+using ActivityManagerAPI.Repositories.Abstract;
+using Microsoft.EntityFrameworkCore;
+
+namespace ActivityManagerAPI.Repositories.Concrete
+{
+    public class ActivityRepository : GenericRepository<Activity>, IActivityRepository
+    {
+        public ActivityRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
